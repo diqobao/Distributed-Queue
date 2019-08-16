c9a71b9edb78112cd606a8a5448c0205599fa84a
diqobao <leshy7@gmail.com>
2019-08-16 17:42:55 -0400

    add dependencies

--- src/main/java/msgQ/consumer/Consumer.java
+++ src/main/java/msgQ/consumer/Consumer.java
@@ -1,27 +1,49 @@
 package msgQ.consumer;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+
+
 public class Consumer {
 
-    /*
+    int consumerID;
+
+    /**
     * Set up a new consumer
     *
-    * */
+    */
     public Consumer() {
 
     }
 
+    /**
+     * Start consumer
+     *
+     */
     public void start() {
 
     }
 
+    /**
+     * Terminate consumer
+     *
+     */
     public void stop() {
 
     }
 
+    /**
+     * Subscribe to topics
+     *
+     */
     public void subscribe() {
 
     }
 
+    /**
+     * Unsubscribe to topics
+     *
+     */
     public void unsubscribe() {
 
     }
